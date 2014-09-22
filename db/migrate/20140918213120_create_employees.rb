e491f068dcc4127d31553856865a16683ce5f9e1
Blaed <blaedj@gmail.com>
2014-09-22 13:23:03 -0500

    employee scaffold

--- db/migrate/20140918213120_create_employees.rb
+++ db/migrate/20140918213120_create_employees.rb
@@ -1,8 +1,6 @@
 class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
-      t.string :name
-      t.dailyreports :has_many
 
       t.timestamps
     end
