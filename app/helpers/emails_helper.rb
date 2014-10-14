98edca615125201fa2eed7d622ccf07fd5c0660a
blaedj <blaedj@gmail.com>
2014-10-14 16:25:56 -0500

    work on the email processing

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -2,7 +2,14 @@ module EmailsHelper
 
 
   def process_email args
-    DailyReport.new(content: args["stripped-text"])
+    email = parse_email(args["Return-Path"])
+    employee = Employee.find_by_email(email)
+    emp_id = employee.nil? ? "" : employee.id
+    DailyReport.new(content: args["stripped-text"], employee_id: emp_id)
+  end
+
+  def parse_email email
+    email.sub(/^</, '').sub(/>$/, '')
   end
 
 end
--- spec/helpers/emails_helper_spec.rb
+++ spec/helpers/emails_helper_spec.rb
@@ -21,18 +21,24 @@ RSpec.describe EmailsHelper, :type => :helper do
         "body-plain"    => "no longer checking the authenticity token when receiving emails\r\n",
         "Date"          => "Mon, 13 Oct 2014 14:38:29 -0500",
         "timestamp"     => "1413229117",
-        "Return-Path"=>"<menge051@d.umn.edu>",
+        "Return-Path"=>"<test@test.com>",
       }
       @daily = process_email(@test_params)
-      @test_employee = FactoryGirl.create(:active_employee)
+      @test_employee = FactoryGirl.create(:active_employee, email: "test@test.com")
     end
 
     it "should get the plain body text" do
       expect(@daily.content).to eql("no longer checking the authenticity token when receiving emails")
     end
     it "should get the sender of the email" do
-      allow(Employee).to receive(:find).and_return(@test_employee)
+      allow(Employee).to receive(:find_by_email).and_return(@test_employee)
+      @daily = process_email(@test_params)
       expect(@daily.employee).to eql(@test_employee)
+    end
+    describe "parse email from return path" do
+      it "should extract the email" do
+        expect(parse_email("<test@test.com>")).to eql("test@test.com")
+      end
     end
 
   end
