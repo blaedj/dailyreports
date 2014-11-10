c3bbe35029dfeadc379726d150c2dcbfe9392a68
blaedj <blaedj@gmail.com>
2014-11-10 14:57:45 -0600

    get the daily_report index and show pages working

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,4 +1,5 @@
 class DailyReportsController < ApplicationController
+  before_action :set_daily_report, only: [:show, :edit, :update, :destroy]
 
   def index
     @daily_reports = DailyReport.all
@@ -11,8 +12,15 @@ class DailyReportsController < ApplicationController
   # GET /reports/1
   # GET /reports/1.json
   def show
-    @daily_report = DailyReport.find(dr_params[:id])
+    #@daily_report = DailyReport.find(dr_params[:id])
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_daily_report
+      @daily_report = DailyReport.find(params[:id])
+    end
+
 
   def dr_params
     params.require(:daily_report).permit(:id, :employee)
