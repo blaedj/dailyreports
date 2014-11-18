758c14856908a6e6c2fae3bb8248b24adc3403cc
blaedj <blaedj@gmail.com>
2014-11-18 14:05:49 -0600

    create helper method to generate link for daily_report

--- app/helpers/daily_reports_helper.rb
+++ app/helpers/daily_reports_helper.rb
@@ -1,7 +1,13 @@
 module DailyReportsHelper
 
-  def link_to_daily_report(date, employee)
-    '/daily_reports/2'
+  def link_to_daily_report(date, employee_id)
+    #TODO: should handle both an employee object and an employee id
+    if(date.nil? or employee_id.nil?)
+      return ''
+    end
+    dr_matches = DailyReport.where('date(date) = ?', date).where('employee_id = ?', employee_id)
+    first_match = dr_matches.first
+    daily_report_path(first_match)
   end
 
 end
--- spec/helpers/daily_reports_helper_spec.rb
+++ spec/helpers/daily_reports_helper_spec.rb
@@ -12,23 +12,18 @@ require 'rails_helper'
 # end
 RSpec.describe DailyReportsHelper, :type => :helper do
   describe "link_to_daily_report given day and employee" do
-    it "should provide a link to view the daily report" do
-      daily_report = FactoryGirl.create(:daily_report)
-      employee =  FactoryGirl.create(:employee)
-      link = link_to_daily_report(daily_report.date, employee.id)
-      expect(link).to eql('/daily_reports/2')
-    end
-    it "should give the correct report for day and employee" do
-      date = Date.today
+    it "should give a link to correct daily report for day and employee" do
+      date = DateTime.now
       employee = FactoryGirl.create(:active_employee)
-      daily_report = FactoryGirl.create(:daily_report, employee_id: employee.id, date: date)
-      expect(link_to_daily_report(date, employee.id)).to eql("/daily_report/#{daily_report.id}")
+      daily_report = FactoryGirl.create(:daily_report, employee_id: employee.id, date: Date.today)
+      expect(link_to_daily_report(date, employee.id)).to eql("/daily_reports/#{daily_report.id}")
     end
   end
-  describe "" do
-
+  describe "link_to_daily_report with nil params" do
+    it "should return an empty string" do
+      expect(link_to_daily_report(nil, nil)).to eql("")
+    end
   end
-
 
 
 end
